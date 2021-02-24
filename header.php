e7a44e510ef73c94b82bca88264197e970884e50
Jacob Weinbren <jacobweinbren@gmail.com>
2021-02-24 19:59:46 +0000

    fixed for mob devices

--- header.php
+++ header.php
@@ -2,8 +2,6 @@
 <html lang="en">
 
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="The Social Review is an inclusive platform which creates a space for grassroots left of centre voices.">
 
@@ -13,6 +11,10 @@
 <body>
 
 	<div class="fullnav">
+		<div class="header">
+			<img src="<?php echo get_bloginfo('template_url') ?>/images/logo.png"/>
+		</div>
+
 		<nav class="navbar">
 			<a href="#" class="nav-item deslogo">
 				<img src="<?php echo get_bloginfo('template_url') ?>/images/logo.png"/>
